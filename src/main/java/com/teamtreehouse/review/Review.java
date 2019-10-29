c0b1d9bd140aa3494706bd1891739dea2ac392b5
Vivian Coday <vivian@spanishowl.com>
2019-10-28 17:47:25 -0700

    Initial commit.

---
+++ src/main/java/com/teamtreehouse/review/Review.java
@@ -0,0 +1,59 @@
+package com.teamtreehouse.review;
+
+import com.teamtreehouse.core.BaseEntity;
+import com.teamtreehouse.course.Course;
+import com.teamtreehouse.user.User;
+
+import javax.persistence.*;
+
+@Entity
+public class Review extends BaseEntity {
+    private int rating;
+    private String description;
+
+    @ManyToOne
+    private Course course;
+    @ManyToOne
+    private User reviewer;
+
+    protected Review() {
+        super();
+    }
+
+    public Review(int rating, String description) {
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public User getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(User reviewer) {
+        this.reviewer = reviewer;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/teamtreehouse/review/ReviewRepository.java
@@ -0,0 +1,6 @@
+package com.teamtreehouse.review;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
+}
